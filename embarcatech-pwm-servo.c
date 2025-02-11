a1cd1ef62103bc245a5d05369fb3c8cf7f6a220c
Matheus Gouveia <gouveiamdb@gmail.com>
2025-02-11 20:05:19 -0300

    Adiciona controle de servo e diagrama de conexão

--- embarcatech-pwm-servo.c
+++ embarcatech-pwm-servo.c
@@ -10,6 +10,8 @@ void setup_pwm() {
     pwm_init(pwm_gpio_to_slice_num(SERVO_PIN), PWM_FREQ, WRAP_COUNT);
     pwm_set_gpio_level(SERVO_PIN, 0);
     pwm_set_enabled(pwm_gpio_to_slice_num(SERVO_PIN), true);
+
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
 }
 
 
@@ -17,8 +19,22 @@ int main() {
     stdio_init_all();
     setup_pwm();
 
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // 180 graus (2400µs)
+        printf("Movendo para 180 graus\n");
+        pwm_set_chan_level(slice_num, PWM_CHAN_A, us_to_count(2400));
+        sleep_ms(5000);
+
+        // 90 graus (1470µs)
+        printf("Movendo para 180 graus\n");
+        pwm_set_chan_level(slice_num, PWM_CHAN_A, us_to_count(2400));
+        sleep_ms(5000);
+
+        // 0 graus (500µs)
+        printf("Movendo para 0 graus\n");
+        pwm_set_chan_level(slice_num, PWM_CHAN_A, us_to_count(500));
+        sleep_ms(5000);
     }
 }
