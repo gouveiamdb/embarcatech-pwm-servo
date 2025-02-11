5e9ecbfda89bf91f5cc5f0842e3592b4c408e88e
Matheus Gouveia <gouveiamdb@gmail.com>
2025-02-11 19:59:25 -0300

    Adiciona a biblioteca hardware_pwm ao projeto

--- embarcatech-pwm-servo.c
+++ embarcatech-pwm-servo.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+#define SERVO_PIN 22
+#define PWM_FREQ 50
+#define WRAP_COUNT 39062
+
+void setup_pwm() {
+    pwm_init(pwm_gpio_to_slice_num(SERVO_PIN), PWM_FREQ, WRAP_COUNT);
+    pwm_set_gpio_level(SERVO_PIN, 0);
+    pwm_set_enabled(pwm_gpio_to_slice_num(SERVO_PIN), true);
+}
 
 
-
-int main()
-{
+int main() {
     stdio_init_all();
+    setup_pwm();
 
     while (true) {
         printf("Hello, world!\n");
