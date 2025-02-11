ce9488102e9dc24fb8a2c042b7564379c9b4735f
Matheus Gouveia <gouveiamdb@gmail.com>
2025-02-11 20:16:23 -0300

    Ajusta configuração do PWM para controle de servo e corrige ângulo de movimento

--- embarcatech-pwm-servo.c
+++ embarcatech-pwm-servo.c
@@ -4,15 +4,23 @@
 
 #define SERVO_PIN 22
 #define PWM_FREQ 50
+#define CLOCK_DIV 64
 #define WRAP_COUNT 39062
 
-void setup_pwm() {
-    pwm_init(pwm_gpio_to_slice_num(SERVO_PIN), PWM_FREQ, WRAP_COUNT);
-    pwm_set_gpio_level(SERVO_PIN, 0);
-    pwm_set_enabled(pwm_gpio_to_slice_num(SERVO_PIN), true);
-
-    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
+uint us_to_count(uint us) {
+    return us * (125000 / CLOCK_DIV) / 1000;
 }
+
+void setup_pwm() {
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
+
+    pwm_set_clkdiv(slice_num, CLOCK_DIV);
+    pwm_set_wrap(slice_num, WRAP_COUNT);
+    
+    pwm_set_enabled(slice_num, true);
+}
+
 
 
 int main() {
@@ -28,8 +36,8 @@ int main() {
         sleep_ms(5000);
 
         // 90 graus (1470µs)
-        printf("Movendo para 180 graus\n");
-        pwm_set_chan_level(slice_num, PWM_CHAN_A, us_to_count(2400));
+        printf("Movendo para 90 graus\n");
+        pwm_set_chan_level(slice_num, PWM_CHAN_A, us_to_count(1470));
         sleep_ms(5000);
 
         // 0 graus (500µs)
